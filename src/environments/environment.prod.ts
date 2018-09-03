611e3d3e8699f290a889aadd9028af870c08d420
njb <njblondhe@gmail.com>
2018-09-03 07:58:23 -0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,6 +11,6 @@ export const environment = {
 export const backend = {
   production: true,
   config: {
-    url: 'http://criticslist.herokuapp.com'
+    url: 'https://criticslist.herokuapp.com'
   }
 };
