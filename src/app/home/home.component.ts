94f93eef55a2a63faa59c97a39b77ba3296d6ae6
njb <njblondhe@gmail.com>
2019-08-29 06:11:19 -0500

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
-import { backend } from '../../environments/environment';
+import { backend, environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +16,13 @@ export class HomeComponent implements OnInit {
   constructor(private _spotify: SpotifyService) { }
 
   ngOnInit() {
+    console.log('is production environment ', environment.production);
+    console.log(this.authURL);
   }
 
   login() {
+    console.log('is production environment ', environment.production);
+    console.log(this.authURL);
     window.location.href = this.authURL + '/auth/login';
   }
 }
